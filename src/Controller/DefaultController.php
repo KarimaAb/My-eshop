d55e149b7eb3d5e00b53e0626b4e75f411372f19
KarimaAb <rima.bekhti@hotmail.com>
2022-07-08 11:01:27 +0200

    end commit

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,20 +2,21 @@
 
 namespace App\Controller;
 
+use App\Entity\Produit;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
 {
-	/**
-	 * Une fonction de controller s'appelle une action
-	 * l'argument "name" de la Route se compose du nom du controller_nom de l'action.
-	 *
-	 * @Route("/", name="default_home", methods={"GET"})
-	 */
-	public function home(): Response
-	{
-		return $this->render('default/home.html.twig');
-	}
+    /**
+     * @Route("/", name="default_home", methods={"GET"})
+     */
+    public function home(EntityManagerInterface $entityManager): Response
+    {
+        return $this->render('default/home.html.twig',[
+            'produits' => $entityManager->getRepository(Produit::class)->findBy(['deletedAt' => null])
+        ]);
+    }
 }
